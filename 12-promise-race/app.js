00624a7f600a2bbeadd809d90fb479ec30dd26c9
Irina Kiriliuk <irinakiriliuk@gmail.com>
2026-05-25 15:39:38 +0200

    Add promise race

---
+++ 12-promise-race/app.js
@@ -0,0 +1,8 @@
+//Создать функцию race(), которая будет принимать массив Promise,
+//  и возвращать первый успешно выполненный или отклоненный.
+
+function race (promises) {
+    return new Promise((resolve, reject) => {
+        
+    })
+}
