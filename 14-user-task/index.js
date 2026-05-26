4f4d3240ffcda7aaf6971a0d548a9c104e8be4f8
Irina Kiriliuk <irinakiriliuk@gmail.com>
2026-05-26 20:25:37 +0200

    Add file 3

--- 14-user-task/index.js
+++ 14-user-task/index.js
@@ -1,4 +1,8 @@
 import Task from './task';
 import User from './user';
 
-const myTask = new Task ('')
+const myTask = new Task ('Задача успешно выполнена!');
+
+const myUser = new User(myTask);
+
+myUser.do();
